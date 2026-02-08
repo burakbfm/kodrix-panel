3c4b57a51eb29d38f84156216c85ad376ed299bd
Burak Üstün <throborexz@gmail.com>
2026-02-08 03:59:03 +0300

    s
    s

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,38 +1,36 @@
-import { createServerClient } from "@supabase/ssr";
-import { type NextRequest, NextResponse } from "next/server";
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export const createClient = (request: NextRequest) => {
-    // Create an unmodified response
-    let response = NextResponse.next({
-        request: {
-            headers: request.headers,
-        },
-    });
+export async function updateSession(request: NextRequest) {
+    let supabaseResponse = NextResponse.next({
+        request,
+    })
 
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
-                    return request.cookies.getAll();
+                    return request.cookies.getAll()
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value }) =>
-                        request.cookies.set(name, value),
-                    );
-                    response = NextResponse.next({
-                        request: {
-                            headers: request.headers,
-                        },
-                    });
+                    cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    })
                     cookiesToSet.forEach(({ name, value, options }) =>
-                        response.cookies.set(name, value, options),
-                    );
+                        supabaseResponse.cookies.set(name, value, options)
+                    )
                 },
             },
-        },
-    );
+        }
+    )
 
-    return { supabase, response };
-};
+    // Refresh session if expired
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    return supabaseResponse
+}
